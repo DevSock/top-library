901ff55f752770712ca459d41137100100907ed8
DevSock <throwaway237429@gmail.com>
2022-09-25 20:55:12 -0400

    Add logic for adding/removing books

--- script.js
+++ script.js
@@ -1,6 +1,72 @@
-const modal = document.getElementById("modal");
-const showModalButton = document.getElementById("show-modal");
-const closeModalButton = document.getElementById("close-modal");
+const parentBookElement = document.getElementById("clone-book");
+const modalElement = document.getElementById("modal");
+const showModalButtonElement = document.getElementById("show-modal");
+const closeModalButtonElement = document.getElementById("close-modal");
+const bookContainerElement = document.getElementById("book-list");
+let bookList = [];
+
+function book(title, author, pagecount, publishdate, readstatus) {
+  this.title = title;
+  this.author = author;
+  this.pagecount = pagecount;
+  this.publishdate = publishdate;
+  this.readstatus = readstatus;
+  this.id = crypto.randomUUID();
+}
+
+function addBookToList(book) {
+  const clonedElement = createClonedElement(book);
+  bookList.push(book);
+  bookContainerElement.appendChild(clonedElement);
+}
+
+function createClonedElement(book) {
+  const cloneElement = parentBookElement.cloneNode(true);
+  const deleteButtonElement = cloneElement.querySelector(
+    ".book-admin .book-delete"
+  );
+  const titleElement = cloneElement.querySelector(".data-list .book-title");
+  const authorElement = cloneElement.querySelector(".data-list .book-author");
+  const pageCountElement = cloneElement.querySelector(
+    ".data-list .book-pagecount"
+  );
+  const publishDateElement = cloneElement.querySelector(
+    ".data-list .book-publishing"
+  );
+  const readStatusElement = cloneElement.querySelector(
+    ".data-list .book-readstatus"
+  );
+  const statusTextElement = readStatusElement.querySelector(".status-text");
+  const hiddenCheckboxElement = readStatusElement.querySelector(
+    "input[type=checkbox]"
+  );
+  const sliderBackgroundElement =
+    readStatusElement.querySelector(".slider-background");
+
+  cloneElement.removeAttribute("hidden");
+  cloneElement.setAttribute("id", book.id);
+  deleteButtonElement.setAttribute("data-book-id", book.id);
+
+  titleElement.textContent = book.title;
+  authorElement.textContent = book.author;
+  pageCountElement.textContent = book.pagecount;
+  publishDateElement.textContent = book.publishdate;
+
+  hiddenCheckboxElement.id = `${book.id}-checkbox`;
+  sliderBackgroundElement.setAttribute("for", `${book.id}-checkbox`);
+
+  if (book.readstatus) {
+    statusTextElement.textContent = "Read";
+    readStatusElement.classList.add("book-read");
+    hiddenCheckboxElement.checked = true;
+  } else {
+    statusTextElement.textContent = "Unread";
+    readStatusElement.classList.remove("book-read");
+    hiddenCheckboxElement.checked = false;
+  }
+
+  return cloneElement;
+}
 
 function showModal() {
   modal.removeAttribute("hidden");
@@ -10,5 +76,23 @@ function closeModal() {
   modal.setAttribute("hidden", true);
 }
 
-showModalButton.addEventListener("click", showModal);
-closeModalButton.addEventListener("click", closeModal);
+function deleteBook(event) {
+  const currentTarget = event.currentTarget.activeElement;
+
+  if (currentTarget.classList.contains("book-delete")) {
+    const uuid = currentTarget.getAttribute("data-book-id");
+    const targetBook = document.getElementById(uuid);
+    const bookListEntry = bookList.find((entry) => {
+      if (entry.id == uuid) {
+        return entry;
+      }
+    });
+
+    targetBook.remove();
+    bookList.splice(bookList.indexOf(bookListEntry), 1);
+  }
+}
+
+document.addEventListener("click", deleteBook);
+showModalButtonElement.addEventListener("click", showModal);
+closeModalButtonElement.addEventListener("click", closeModal);
